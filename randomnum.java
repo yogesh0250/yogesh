5f494dd95c5023d8e9c2ade80b55603059eede30
yogesh0250 <yogeshsundararaj92@gmail.com>
2024-09-17 15:21:11 +0530

    Add files via upload

---
+++ randomnum.java
@@ -0,0 +1,7 @@
+public class randomnum {
+    public static void main(String[] args) {
+      int randomNum = (int)(Math.random() * 101);  // 1 to 100
+      System.out.println(randomNum);
+    }
+  }
+  
